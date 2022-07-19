c585afebb0a7ee0188ed72da0384deb51faccd60
drattylight <mlaux6@outlook.com>
2022-07-19 15:52:44 -0500

    Add signup page

--- components/forms/signupForm.tsx
+++ components/forms/signupForm.tsx
@@ -10,7 +10,11 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
-const SignupForm = () => {
+interface deviceType {
+  deviceType: string;
+}
+
+const SignupForm = (props: deviceType) => {
   const router = useRouter();
 
   const handleSignupInfo = async (event: React.FormEvent) => {
@@ -18,12 +22,9 @@ const SignupForm = () => {
     const formValidation = (await import('../../lib/formValidation')).default;
     const validated = formValidation(event);
     if (validated) {
-      const getCookieValue = (await import('../../lib/getCookieValue')).default;
-      const csrfCookieValue = getCookieValue('_csrf');
-      if (typeof csrfCookieValue !== 'string') return;
       type SignupDetails = EventTarget & {
-        firstname: HTMLInputElement;
-        lastname: HTMLInputElement;
+        firstName: HTMLInputElement;
+        lastName: HTMLInputElement;
         email: HTMLInputElement;
         password: HTMLInputElement;
       };
@@ -31,31 +32,98 @@ const SignupForm = () => {
       // add error / timeout for fetch
       const res = await fetch('http://localhost:4000/api/auth/register', {
         body: JSON.stringify({
-          firstname: target.firstname.value,
-          lastname: target.lastname.value,
+          first_name: target.firstName.value,
+          last_name: target.lastName.value,
           email: target.email.value,
           password: target.password.value,
         }),
         credentials: 'same-origin',
         headers: {
           'Content-Type': 'application/json',
-          'CSRF-Token': csrfCookieValue,
           'X-Custom-Header': 'lollipop',
         },
         method: 'POST',
       });
       (event.target as HTMLFormElement).reset();
       const result = await res.json();
-      router.push('/login');
+      router.push('/app/dashboard');
     }
   };
 
-  return (
+  return props.deviceType === 'mobile' ? (
     <form
       onSubmit={handleSignupInfo}
       className="form needs-validation rounded"
       noValidate
-      id="signupForm"
+      id="mobileForm"
+    >
+      <label htmlFor="firstName" className="form-label">
+        First Name
+      </label>
+      <input
+        id="firstNameMobile"
+        type="text"
+        autoComplete="given-name"
+        required
+        className="form-control"
+      />
+      <div className="invalid-feedback">This field is required.</div>
+      <label htmlFor="lastName" className="form-label mt-3">
+        Last Name
+      </label>
+      <input
+        id="lastNameMobile"
+        type="text"
+        autoComplete="family-name"
+        required
+        className="form-control"
+      />
+      <div className="invalid-feedback">This field is required.</div>
+      <label htmlFor="email" className="form-label mt-3">
+        Email
+      </label>
+      <input
+        id="emailMobile"
+        type="email"
+        autoComplete="email"
+        required
+        aria-describedby="emailHelp"
+        className="form-control"
+      />
+      <div id="emailHelp" className="form-text text-secondary">
+        We will never share your email.
+      </div>
+      <div className="invalid-feedback">This field is required.</div>
+      <div className="invalid-feedback">
+        Please ensure email is of form johnsmith@email.com
+      </div>
+      <label htmlFor="password" className="form-label pt-3">
+        Password
+      </label>
+      <input
+        id="passwordMobile"
+        type="password"
+        required
+        aria-describedby="passwordHelp"
+        className="form-control"
+        minLength={8}
+      />
+      <div id="passwordHelp" className="form-text text-secondary">
+        Must be at least 8 characters long.
+      </div>
+      <div className="invalid-feedback">This field is required.</div>
+      <div className="row text-center ms-4 me-4">
+        <button type="submit" className="btn btn-warning mt-4">
+          Submit Info
+        </button>
+      </div>
+    </form>
+  ) : (
+    <form
+      onSubmit={handleSignupInfo}
+      className="form needs-validation rounded"
+      noValidate
+      id="desktopForm"
     >
       <label htmlFor="firstName" className="form-label">
         First Name
@@ -104,10 +172,11 @@ const SignupForm = () => {
         id="password"
         type="password"
         required
+        aria-describedby="passwordHelp"
         className="form-control"
         minLength={8}
       />
-      <div id="emailHelp" className="form-text text-secondary">
+      <div id="passwordHelp" className="form-text text-secondary">
         Must be at least 8 characters long.
       </div>
       <div className="invalid-feedback">This field is required.</div>
